b561e587f0f3c11dcbb6b3af6ccbc0859638d329
qingyang <qingyang@192.168.0.219>
2020-10-17 16:40:05 +0800

    Created item1 and prompted user for item1

--- LectureGit/src/ItemToPurchase.java
+++ LectureGit/src/ItemToPurchase.java
@@ -1,4 +1,6 @@
 
+import java.util.*;
+
 public class ItemToPurchase {
 
 	String itemName;
@@ -25,4 +27,17 @@ public class ItemToPurchase {
 		this.itemQuantity = itemQuantity;
 	}
 	
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		ItemToPurchase item1 = new ItemToPurchase();
+		System.out.println("Enter the item name: ");
+		item1.itemName = scnr.nextLine();
+		System.out.println("Enter the item price: ");
+		item1.itemPrice = scnr.nextInt();
+		System.out.println("Enter the item quantity: ");
+		item1.itemQuantity = scnr.nextInt();
+
+		
+
+	}
 }
